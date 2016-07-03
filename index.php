e19f8581ca2dd8ab0995f2f12dd0c9cfb5637c76
RaiZeN <daniel.wygonski@gmail.com>
2016-07-03 22:26:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
-Cat: <input type="text" id="cat" onkeyup="refresh();" /> <span id="cat2"></span>
+Category:
+<select id="cat" onchange="refresh();">
+	<option value="cars" selected="selected">cars</option>
+	<option value="films">films</option>
+	<option value="games">games</option>
+	<option value="girls">girls</option>
+</select>
+
 <br />
-Bg: <input type="text" id="bg" onkeyup="refresh();" />
+
+Background:
+<select id="bg" onchange="refresh();">
+	<option value="bg1">bg1</option>
+	<option value="bg2">bg2</option>
+	<option value="bg3">bg3</option>
+	<option value="bg4">bg4</option>
+	<option value="bg5" selected="selected">bg5</option>
+</select>
+
 <br / >
 Nick: <input type="text" id="nick" onkeyup="refresh();" />
 <br>
@@ -11,8 +27,13 @@ Nick: <input type="text" id="nick" onkeyup="refresh();" />
 	
 	function refresh()
 	{
-		document.getElementById('img').src = 'img.php?bg=' + document.getElementById('bg').value + '&nick=' + document.getElementById('nick').value + '&cat=' + document.getElementById('cat').value;
+		var e = document.getElementById("cat");
+		var cat = e.options[e.selectedIndex].value;
+
+		document.getElementById('img').src = 'img.php?bg=' + document.getElementById('bg').value + '&nick=' + document.getElementById('nick').value + '&cat=' + cat;
 	}
+
+	refresh();
 
 
 </script>
