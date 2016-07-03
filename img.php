917c8504dacd8c2a22f0a761f51e5c3412c5d035
MrRaiZeN <wygonski@gmail.com>
2016-07-03 20:55:57 +0200

    #1

---
+++ img.php
@@ -0,0 +1,22 @@
+<?php
+
+	// created by RaiZeN
+	// @xRaiZeN_ | /id/xRaiZeN | /u/MrRaiZeN
+
+	// image 
+	$img = ImageCreateFrompng('img/bg/'.$_GET['cat'].'/'.$_GET['bg'].'.png');
+	
+	// rgb colors
+	$white = ImageColorAllocate($img, 255, 255, 255);
+
+	// fonts 
+	$font = "font/font.otf";
+	
+	// adding text (nickname)
+	imagettftext($img, 10, 0, 235, 155, $white, $font, $_GET['nick']);
+	
+	// saving alpha and image
+	imagesavealpha($img,true);
+	header("Content-type: image/png");
+	ImagePng($img);
+?>
